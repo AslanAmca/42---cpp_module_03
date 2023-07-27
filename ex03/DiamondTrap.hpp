cedd70b9d01b1c311e11abcb07d77402dd04c0da
Ali Aslan <aliaslan_61@outlook.com>
2023-07-27 23:36:24 +0300

    hepsi tamam gibi

---
+++ ex03/DiamondTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaslan <aaslan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 16:44:42 by aaslan            #+#    #+#             */
+/*   Updated: 2023/07/27 21:22:03 by aaslan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+class DiamondTrap : public ScavTrap, public FragTrap
+{
+private:
+	std::string name;
+
+public:
+	DiamondTrap(void);
+	DiamondTrap(std::string nameValue);
+	DiamondTrap(const DiamondTrap &other);
+	DiamondTrap &operator=(const DiamondTrap &other);
+	~DiamondTrap(void);
+
+	using ScavTrap::attack;
+	void whoAmI(void);
+};
+
+#endif
